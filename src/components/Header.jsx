b699184a89e0c3b3c5c004196ae5c826e2da4eaf
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-06 13:43:53 -0700

    Deployment Pipeline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <header className="header bg-dark">
             <nav className="navbar navbar-expand-lg navbar-dark">
                 <div className="container">
-                    <Link className="navbar-brand" to="/">VisionML</Link>
+                    <Link className="navbar-brand" to="/">Opti-Neural Technologies</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                         <span className="navbar-toggler-icon"></span>
                     </button>
