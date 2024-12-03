6f850ff808b8e6cf4494a4d32e7e6b5167700015
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-12-03 00:06:14 -0800

    Auth pages done

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -42,7 +42,7 @@ const AuthPage = () => {
         setError('');
 
         try {
-            const response = await fetch('https://backendimagebuddy.onrender.com//api/auth/login', {
+            const response = await fetch('https://backendimagebuddy.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
